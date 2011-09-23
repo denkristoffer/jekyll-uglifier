7fa70bee9934c2f10ef8fc9f5b4cd613be3a0f94
Kristoffer <git@kristoffer.is>
2011-09-23 15:57:37 +0200

    Add converter and a short readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jekyll-Uglifier
+
+## Installation
+
+Put it in your _plugins folder in the main directory for your Jekyll site and make sure you have the Uglifier gem installed.
---
+++ js_uglifier.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  require 'uglifier'
+
+  class JSUglifier < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /js/i
+    end
+
+    def output_ext(ext)
+      ".js"
+    end
+
+    def convert(content)
+      begin
+        engine = Uglifier.new() # Settings go here
+        engine.compile(content)
+      rescue StandardError => e
+        puts "Uglifier Error: " + e.message
+      end
+    end
+  end
+end
